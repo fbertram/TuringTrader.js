24fbd7a8c20d888a5bc697514d66834d2190472f
Felix Bertram <felix@turingtrader.org>
2021-05-01 13:43:02 -0700

    add user settings

---
+++ src/tests/user-settings.js
@@ -0,0 +1,14 @@
+//==============================================================================
+// Name:        tests/user-settings
+// Project:     TuringTrader.js
+// Description: settings used for tests. Note that these might be adjusted
+//              for your specific user/ setup.
+// History:     FUB, 2021v01, created
+//==============================================================================
+
+export const userSettings = {
+    tiingoApiKey: "put your API key here",
+}
+
+//==============================================================================
+// end of file
