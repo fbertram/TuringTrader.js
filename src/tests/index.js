87a7902e68333871f807e8aa7eae0b2da8820845
Felix Bertram <felix@turingtrader.org>
2021-05-02 13:19:33 -0700

    tighten code

--- src/tests/index.js
+++ src/tests/index.js
@@ -10,13 +10,18 @@ import { test_02_calendar } from "./test-02-calendar"
 import { test_03_cache } from "./test-03-cache"
 import { test_04_asset } from "./test-04-asset"
 
+const allTests = [
+    test_01_hello,
+    test_02_calendar,
+    test_03_cache,
+    test_04_asset,
+]
+
 const runTests = () => {
-    if (true) {
-        test_01_hello()
-        test_02_calendar()
-        test_03_cache()
-    }
-    test_04_asset()
+    allTests.forEach(t => {
+        console.log(`========== ${t.name} ==============================`)
+        t()
+    })
 }
 
 runTests()
