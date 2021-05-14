efc68a45b595663cbb12422381c84d8630c679a9
Felix Bertram <felix@turingtrader.org>
2021-05-14 14:53:53 -0700

    cleanup

--- src/tests/test-09-child-strategy.js
+++ src/tests/test-09-child-strategy.js
@@ -9,7 +9,7 @@ import { createSimulator } from "../simulator"
 
 export const test_09_child_strategy = async () => {
     // this tests shows how to run
-    // a child strategy
+    // a simulation with child strategies
 
     const childSpy = {
         run: async (sim) => {
@@ -25,11 +25,10 @@ export const test_09_child_strategy = async () => {
         report: () => {}
     }
 
+    const simSpy = createSimulator(childSpy)
+
     const childAgg = {
         run: async (sim) => {
-            // note that we are typically not
-            // specifying startDate and endDate
-
             sim.deposit(1e6)
 
             await sim.loop(async () => {
@@ -39,41 +38,32 @@ export const test_09_child_strategy = async () => {
         report: () => {}
     }
 
+    const simAgg = createSimulator(childAgg)
+
     const algo = {
         run: async (sim) => {
             sim.startDate = new Date("01/01/2020")
             sim.endDate = new Date("12/31/2020")
             sim.deposit(1e6)
 
-            // note how we are creating child algorithms
-            // and use them as assets later on
-
-            const spy = createSimulator(childSpy)
-            const agg = createSimulator(childAgg)
-
             await sim.loop(async () => {
                 const orders = []
 
-                // note how we are rebalancing on a monthly
-                // schedule by looking at the simulator's
-                // next timestamp 
-
                 if (sim.t(0).getMonth() !== sim.t(-1).getMonth()) {
 
-                    // note how orders are first added to a basket
-                    // and then returned to the simulator
+                    // note how we are using a child simulator 
+                    // just like any other asset
 
-                    orders.push(sim.asset(spy).alloc(0.6, sim.orderTypes.mktNextOpen))
-                    orders.push(sim.asset(agg).alloc(0.4, sim.orderTypes.mktNextOpen))
+                    orders.push(sim.asset(simSpy).alloc(0.6, sim.orderTypes.mktNextOpen))
+                    orders.push(sim.asset(simAgg).alloc(0.4, sim.orderTypes.mktNextOpen))
                 }
 
                 return orders
             })
 
-            // BUGBUG: this should really go into report.
-            // we need to add some more awaits to make sure
-            // report is not called before the simulation
-            // loop finished
+            // note how this simulator shows an asset allocation
+            // with the child simulators resolved to the underlying
+            // basic assets
 
             sim.info(sim.getProperty("result"))
         },
