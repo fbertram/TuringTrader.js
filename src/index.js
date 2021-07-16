5edb52ff2e7d867ce348866bed890adc84688af7
Felix Bertram <felix@turingtrader.org>
2021-07-15 17:31:39 -0700

    include Babel runtime

--- src/index.js
+++ src/index.js
@@ -5,13 +5,6 @@
 // History:     FUB, 2021vii13, created
 //==============================================================================
 
-// FIXME: this is required because of using Babel?
-// without it, we see an error 'ReferenceError: regeneratorRuntime is not defined',
-// which seems to be tied to usage of async/ await
-// see here https://babeljs.io/docs/en/usage#polyfill
-//import "core-js/stable";
-import "regenerator-runtime/runtime";
-
 export { createSimulator } from "./simulator/sim-core"
 export { createReport } from "./reports/metrics"
 
