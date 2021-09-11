92a2267befc5752d788a8f26434cca630391f8b2
Felix Bertram <felix@turingtrader.org>
2021-09-11 08:02:13 -0700

    implement more metrics

--- __tests__/test-0600-metrics.js
+++ __tests__/test-0600-metrics.js
@@ -11,7 +11,7 @@ import { createSimulator, createReport } from "../src"
 
 const algo = {
     run: (sim) => {
-        sim.startDate = new Date("2020-01-01T18:00:00.000-05:00") // 6pm in America/New York (winter)
+        sim.startDate = new Date("2007-01-01T18:00:00.000-05:00") // 6pm in America/New York (winter)
         sim.endDate = new Date("2020-12-31T18:00:00.000-05:00") // 6pm in America/New York (winter)
 
         return sim.asset("^gspc").data // S&P 500 index
@@ -28,14 +28,21 @@ describe("test 5000: simple report", () => {
             .then((result) => createReport(result))
             .then((report) => {
                 const metrics = report.metrics
-                expect(metrics.firstBar).toEqual(new Date("2019-12-31T21:00:00.000Z"))
+                //console.log(metrics)
+                expect(metrics.firstBar).toEqual(new Date("2006-12-29T21:00:00.000Z"))
                 expect(metrics.lastBar).toEqual(new Date("2020-12-31T21:00:00.000Z"))
-                expect(metrics.reportDays).toEqual(366)
-                expect(metrics.reportYears).toBeCloseTo(0.99931, 2)
-                expect(metrics.startValue).toBeCloseTo(3215.1799, 1)
+                expect(metrics.reportDays).toEqual(5116)
+                expect(metrics.reportYears).toBeCloseTo(14.0068, 2)
+                expect(metrics.startValue).toBeCloseTo(1424.7099, 1)
                 expect(metrics.endValue).toBeCloseTo(3756.0700, 1)
-                expect(metrics.cagr).toBeCloseTo(16.7858, 2)
-                expect(metrics.mdd).toBeCloseTo(33.9250, 2)
+                expect(metrics.cagr).toBeCloseTo(7.1661, 2)
+                expect(metrics.stdev).toBeCloseTo(18.6480, 2)
+                expect(metrics.mdd).toBeCloseTo(56.7754, 2)
+                expect(metrics.mfd).toEqual(1997)
+                expect(metrics.ulcer).toBeCloseTo(16.3702, 2)
+                expect(metrics.sharpe).toBeCloseTo(0.3843, 2)
+                expect(metrics.martin).toBeCloseTo(0.4378, 2)
+
             })
     })
 })
--- src/reports/metrics.js
+++ src/reports/metrics.js
@@ -19,29 +19,103 @@ export const createReport = (simResult) => {
             const reportYears =
                 (lastBar.getTime() - firstBar.getTime()) / yearInMs
 
-            //----- CAGR
+            //----- compound annual growth rate
             const startValue = simResult.o[0]
             const endValue = simResult.c[simResult.c.length - 1]
             const cagr =
                 100 * (Math.pow(endValue / startValue, 1 / reportYears) - 1)
 
-            //----- drawdown
-            let maxClose = 0
-            const mdd = simResult.c.reduce((prevMdd, newClose) => {
-                maxClose = Math.max(maxClose, newClose)
-                return Math.max(prevMdd, 100 * (1 - newClose / maxClose))
-            }, 0.0)
+            //----- month-to-month returns
+            const calcMonthlyRets = (simResult) => {
+                const monthlyRets = []
+                let prevTime = simResult.t[0]
+                let prevNav = null
+
+                for (let i = 0; i < simResult.t.length; i++) {
+                    const curTime = simResult.t[i]
+                    const curNav = simResult.c[i]
+
+                    if (curTime.getMonth() !== prevTime.getMonth()) {
+                        if (prevNav !== null)
+                            monthlyRets.push(Math.log(curNav / prevNav))
+
+                        prevTime = curTime
+                        prevNav = curNav
+                    }
+                }
+
+                return monthlyRets
+            }
+            const monthlyRets = calcMonthlyRets(simResult)
+
+            //----- standard deviation of returns
+            const avgMonthlyRet = monthlyRets.reduce((sum, ret) => sum + ret, 0.0) / monthlyRets.length
+            const stdevMonthly = Math.sqrt(monthlyRets.reduce((sum2, ret) => sum2 + Math.pow(ret - avgMonthlyRet, 2), 0.0) / monthlyRets.length)
+            const stdev = 100.0 * Math.sqrt(12.0) * stdevMonthly
+
+            //----- maximum drawdown
+            const calcMdd = (simResult) => {
+                let maxClose = 0
+                return simResult.c.reduce((prevMdd, newClose) => {
+                    maxClose = Math.max(maxClose, newClose)
+                    return Math.max(prevMdd, 100 * (1 - newClose / maxClose))
+                }, 0.0)    
+            }
+            const mdd = calcMdd(simResult)
+
+            //----- maximum flat days
+            const calcMaxFlat = (simResult) => {
+                let maxFlat = 0
+                let maxIdx = 0
+
+                for (const idx in simResult.t) {
+                    const t = simResult.t[idx]
+                    const nav = simResult.c[idx]
+
+                    if (nav >= simResult.c[maxIdx]) {
+                        maxFlat = Math.max(maxFlat, (t.getTime() - simResult.t[maxIdx].getTime()) / dayInMs)
+                        maxIdx = idx
+                    }
+                }
+
+                return maxFlat
+            }
+            const maxFlat = calcMaxFlat(simResult)
+
+            //----- ulcer index
+            const calcUlcer = (simResult) => {
+                let maxNav = 0
+                let sumDd2 = 0
+                for (const nav of simResult.c) {
+                    maxNav = Math.max(maxNav, nav)
+                    const dd = 1.0 - nav / maxNav
+                    sumDd2 += dd * dd
+                }
+
+                return Math.sqrt(sumDd2 / simResult.c.length)
+            }
+            const ulcer = 100.0 * calcUlcer(simResult)
+
+            //----- sharpe ratio
+            const sharpe = cagr / stdev
+
+            //----- martin ratio (ulcer performance index)
+            const martin = cagr / ulcer
 
             return {
                 firstBar,
                 lastBar,
                 reportDays,
                 reportYears,
-
                 startValue,
                 endValue,
                 cagr,
+                stdev,
                 mdd,
+                ulcer,
+                mfd: maxFlat,
+                sharpe,
+                martin,
             }
         },
         get chartData() {
